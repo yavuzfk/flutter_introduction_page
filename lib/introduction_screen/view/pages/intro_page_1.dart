c0d12539a9774eb60cf6022734351f9b340a2075
= <=>
2022-11-25 11:37:10 +0300

    Text updated

--- lib/introduction_screen/view/pages/intro_page_1.dart
+++ lib/introduction_screen/view/pages/intro_page_1.dart
@@ -9,7 +9,7 @@ class IntroPage1 extends StatelessWidget {
     const String imagePath = 'assets/flying_around.png';
     const String title = 'Lorem Ipsum is simply \ndummy text of the';
     const String text =
-        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys ';
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys. ';
 
     return const Scaffold(
       body: SafeArea(
